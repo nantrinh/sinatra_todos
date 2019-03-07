7ff19cd8f7db324b9ff7fcff0323dcdf902b69bc
Nancy Trinh <nancytrinh20@gmail.com>
2019-03-07 14:29:48 -0800

    Initial commit

---
+++ todo.rb
@@ -0,0 +1,40 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get "/" do
+  redirect "/lists"
+end
+
+# View list of lists
+get "/lists" do
+  @lists = session[:lists]
+  erb :lists, layout: :layout
+end
+
+# Render the new list form
+get "/lists/new" do
+  erb :new_list, layout: :layout
+end
+
+# Create a new list
+post '/lists' do
+  list_name = params[:list_name].strip
+  if (1..100).cover?(list_name.size)
+    session[:lists] << {name: list_name, todos: []}
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  else
+    session[:error] = "List name must be between 1 and 100 characters."
+    erb :new_list, layout: :layout
+  end
+end
