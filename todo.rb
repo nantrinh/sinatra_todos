6b0aa562d3bf13051d7fbd2d0313214260e62caf
Nancy Trinh <nancytrinh20@gmail.com>
2019-03-07 22:33:09 -0800

    Add todos

--- todo.rb
+++ todo.rb
@@ -27,23 +27,16 @@ get '/lists/new' do
   erb :new_list, layout: :layout
 end
 
-get '/lists/:id' do
-  @list = session[:lists][params[:id].to_i]
+get '/lists/:list_id' do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
   erb :list, layout: :layout
 end
 
-get '/lists/:id/edit' do
-  @list = session[:lists][params[:id].to_i]
+get '/lists/:list_id/edit' do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
   erb :edit_list, layout: :layout
-end
-
-# Return an error message if the name is invalid. Return nil if name is valid.
-def error_for_list_name(name)
-  if !(1..100).cover?(name.size)
-    'List name must be between 1 and 100 characters.'
-  elsif session[:lists].any? { |list| list[:name] == name }
-    'List name must be unique.'
-  end
 end
 
 # Create a new list
@@ -61,10 +54,10 @@ post '/lists' do
 end
 
 # Update list
-post '/lists/:id' do
+post '/lists/:list_id' do
   list_name = params[:list_name].strip
-  id = params[:id].to_i
-  @list = session[:lists][id]
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
 
   error = error_for_list_name(list_name)
   if error
@@ -73,6 +66,47 @@ post '/lists/:id' do
   else
     @list[:name] = list_name
     session[:success] = 'The list has been updated.'
-    redirect "/lists/#{id}"
+    redirect "/lists/#{@list_id}"
+  end
+end
+
+# Delete list
+post '/lists/:list_id/delete' do
+  @list_id = params[:list_id].to_i
+  session[:lists].delete_at(@list_id)
+  session[:success] = "The list has been deleted."
+  redirect "/lists"
+end
+
+# Add todo item to a list
+post '/lists/:list_id/todos' do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+  todo_name = params[:todo].strip
+
+  error = error_for_todo_name(todo_name)
+  if error
+    session[:error] = error
+    erb :list, layout: :layout
+  else
+    @list[:todos] << {name: params[:todo], completed: false} 
+    session[:success] = "The todo item has been added."
+    redirect "/lists/#{@list_id}"
+  end
+end
+
+# Return an error message if the name is invalid. 
+# Return nil if name is valid.
+def error_for_list_name(name)
+  if !(1..100).cover?(name.size)
+    'List name must be between 1 and 100 characters.'
+  elsif session[:lists].any? { |list| list[:name] == name }
+    'List name must be unique.'
+  end
+end
+
+def error_for_todo_name(name)
+  if !(1..100).cover?(name.size)
+    'Todo must be between 1 and 100 characters.'
   end
 end
